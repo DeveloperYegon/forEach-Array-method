e39c7050dc182495a99bade334fdb820b6b9f7e2
developeryegon <gideonyegon404@gmail.com>
2024-01-09 14:02:40 +0300

    have just implemented a forEach() method

---
+++ script.js
@@ -0,0 +1,30 @@
+//array initialisation
+const numbers =[1,2,3,4,5];
+console.log(numbers);
+//array methods
+//=================================forEach() method============================================
+        //displaying items in an array
+numbers.forEach((item,index,arr)=> console.log(item));
+
+        //displaying indexes
+numbers.forEach((item,index,arr)=> console.log(index)); 
+
+        //displaying Array
+numbers.forEach((item,index,arr)=> console.log(arr)); 
+
+        //Adding numbers in an array
+let sum =0;
+numbers.forEach(item=> sum+=item);
+console.log(sum);
+
+        //counting occurence of a letter in an array
+const letters=["a","a","b","c","d","b"];
+let count={};
+letters.forEach(item=>{
+    if(count[item]){
+        count[item]++;
+    }else{
+        count[item]=1;
+    }
+});
+console.log(count);
